f430cad07aba8d6200b20a5cb2d5564fe6701806
Graeme Read <graeme@sigma957.net>
2024-12-01 10:33:00 +0000

    Complete `enum` exercises

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -7,6 +7,11 @@ struct Point {
 #[derive(Debug)]
 enum Message {
     // TODO: Define the different variants used below.
+    Resize { width: usize, height: usize },
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 impl Message {
